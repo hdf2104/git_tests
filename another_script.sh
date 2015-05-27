da656cbc497ab0b37d6ff528b12bddea318ea2db
Haley Fica <haley.fica@gmail.com>
2015-05-27 10:05:02 -0400

    added another loop

--- another_script.sh
+++ another_script.sh
@@ -1,3 +1,21 @@
-for i in 1 2 3;
- do touch file$i.txt;
- done
+#Create one file and  
+
+for i in {1..100000};
+do
+if [ $i == 5 ] then
+ touch file$i.txt
+fi
+done
+
+echo $i
+
+#put some contents into file.
+
+for j in {1..10};
+do
+if [ $j == 5 ] then
+echo "contents of file $j" > file$j.txt
+fi
+done
+
+echo $j
